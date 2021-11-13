8e24acab1f2fc2369a7de8a7c842d4bbb37aeadd
tMinamiii <otherthan.new@gmail.com>
2021-11-13 10:19:48 +0900

    conc

--- gor/main.go
+++ gor/main.go
@@ -2,20 +2,31 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
+
+const concurrency = 5
 
 func main() {
 	c := make(chan string)
-	done := make(chan struct{})
-	go worker(c, done)
+	var wg sync.WaitGroup
+	for i := 0; i < concurrency; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for msg := range c {
+				fmt.Println(msg)
+			}
+		}()
+	}
 	for i := 1; i < 10; i++ {
 		c <- fmt.Sprintf("hello%d", i)
 	}
 	close(c)
-	<-done
+	wg.Wait()
 	fmt.Println("complete")
-
 }
+
 func worker(c chan string, done chan struct{}) {
 	for msg := range c {
 		fmt.Println(msg)
