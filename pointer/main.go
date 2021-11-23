96946e73ff57f913ae0fe906d368f6a5eee70e84
tMinamiii <otherthan.new@gmail.com>
2021-11-23 22:02:13 +0900

    ptr

--- pointer/main.go
+++ pointer/main.go
@@ -4,21 +4,34 @@ import (
 	"fmt"
 )
 
-type A struct {
-	name string
-}
+type A struct{}
 
 type As []A
 
-func Load() *As {
-	var a *As
+func LoadPtr() *As {
+	var a As
+	return &a
+}
+
+func Load() As {
+	var a As
 	return a
 }
 
 func main() {
+	aPtr := LoadPtr()
+	fmt.Printf("aPtr: %v\n", aPtr)                         // &[]
+	fmt.Printf("*aPtr: %v\n", *aPtr)                       // []
+	fmt.Printf("(aPtr == nil): %v\n", aPtr == nil)         // false
+	fmt.Printf("(len(*aPtr) == 0): %v\n", len(*aPtr) == 0) // true
+
 	a := Load()
-	fmt.Println(a)
-	fmt.Println(*a)
-	fmt.Println(a == nil)
-	fmt.Println(len(*a) == 0)
+	fmt.Println(a)           // []
+	fmt.Println(a == nil)    // true
+	fmt.Println(len(a) == 0) // true
+
+	var n []int
+	fmt.Println(n)           // []
+	fmt.Println(n == nil)    // true
+	fmt.Println(len(n) == 0) // true
 }
